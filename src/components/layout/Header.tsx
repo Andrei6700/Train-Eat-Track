9e980869b66ef0be1e2ccdfaa3e611e57c77b6ac
Andrei Balanoiu <andreibalanoiu67@gmail.com>
2026-03-20 17:03:44 +0200

    improvements on ui/ux

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,54 +1,56 @@
-import { colors } from '@/constants/theme';
-import { HeaderProps } from '@/src/types/index';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Typo from '../ui/Typo';
+import { colors } from "@/constants/theme";
+import { HeaderProps } from "@/src/types/index";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Typo from "../ui/Typo";
 
-const Header = ({title= "", leftIcon, rightIcon, style}: HeaderProps) => {
+const Header = ({ title = "", leftIcon, rightIcon, style }: HeaderProps) => {
   return (
     <View style={[styles.container, style]}>
-      {leftIcon &&<View style={styles.leftIcon}>{leftIcon}</View>}
+      {leftIcon && <View style={styles.leftIcon}>{leftIcon}</View>}
       {title && (
-        <Typo size={32} 
-        variant="heading"
-        color={colors.textPrimary}
-        style={{
-            textAlign: 'center', 
+        <Typo
+          size={32}
+          variant="heading"
+          color={colors.textPrimary}
+          style={{
+            textAlign: "center",
             width: leftIcon || rightIcon ? "76%" : "100%",
-            }}>
-                {title}
+          }}
+        >
+          {title}
         </Typo>
-        )}
+      )}
       {rightIcon && <View style={styles.rightIcon}>{rightIcon}</View>}
     </View>
   );
 };
 
-export default Header
+export default Header;
 
 const styles = StyleSheet.create({
-    container: {
-        width: '100%',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        minHeight: 52,
-        backgroundColor: colors.surface,
-        borderBottomWidth: 1,
-        borderBottomColor: colors.border,
-        paddingHorizontal: 12,
-        paddingVertical: 6,
-    },
-    leftIcon: {
-        alignSelf: "flex-start",
-        minWidth: 48,
-        minHeight: 48,
-        justifyContent: "center",
-    },
-    rightIcon: {
-        alignSelf: 'flex-start',
-        minWidth: 48,
-        minHeight: 48,
-        justifyContent: "center",
-    },
-})
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    minHeight: 52,
+    backgroundColor: colors.surface,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.border,
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+  },
+  leftIcon: {
+    alignSelf: "flex-start",
+    minWidth: 48,
+    minHeight: 48,
+    justifyContent: "center",
+  },
+  rightIcon: {
+    alignSelf: "flex-start",
+    minWidth: 48,
+    minHeight: 48,
+    justifyContent: "center",
+  },
+});
